6ec2a485b077a32b2fd8a9b61ad4b9b0d5fc6060
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2023-10-17 21:44:35 +0200

    spring boot

---
+++ springboot/joyfe/src/test/java/com/joyfe/daw/joyfe/JoyfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyfe.daw.joyfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
