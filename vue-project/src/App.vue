9ecda105732bbd2cf761c8839f7b09e7af0113e5
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2024-01-22 18:54:27 +0100

    vie helloworld

---
+++ vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+let Mycount = 0;
+
+function add() {
+  Mycount++;
+  return Mycount;
+}
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" v-on:add="add" />
+
+      <HelloWorld msg="You did it!" v-on:add="add" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    mensaje: String,
+    logo: String,
+    add: ()=>Number,
+  },
+  data() {
+    let jamon = 0;
+    return {
+      jamon,
+      Myadd: this.add
+    }
+  },
+  methods: {
+    add() {
+      this.$emit('add')
+      console.log(this.add);
+      if (this.Myadd != undefined) {
+        this.jamon = this.Myadd();
+      }
+    }
+  }
+}
+let jamon = 0;
+</script>
+
+<template>
+  <div class="hello">
+    <img alt="Vue logo" src="@/assets/logo.svg">
+    <h1>{{ mensaje }}</h1>
+    <button @click="add">Contador: {{jamon}}</button>
+
+  </div>
+</template>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
